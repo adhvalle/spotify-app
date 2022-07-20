bf40c805abef860fdaef41cbd78b2c9646638fd6
Andres Diaz <andres@finhubtechnologies.com>
2022-07-20 14:02:28 +0100

    manage auth session from Spotify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,103 +1,18 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const accessToken = urlParams.get('access_token');
-const refreshToken = urlParams.get('refresh_token');
-
-console.log(accessToken);
-console.log(refreshToken);
-
-if (refreshToken) {
-  console.log('1')
-  fetch(`/api/refresh_token?refresh_token=${refreshToken}`)
-    .then(res => res.json())
-    .then(data => console.log(data));
-
-  console.log('1')
-}
-
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div>
+    <div v-if="accessToken">
+      <p>Logged in</p>
+      <button @click="logout">log out</button>
     </div>
-  </header>
-
-  <RouterView />
+    <div v-else>
+      <a href="http://localhost:8888/login">Log in to Spotify</a>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import { accessToken, logout } from '@/use/spotify.js'
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- index.js
+++ index.js
@@ -64,26 +64,18 @@ app.get('/callback', (req, res) => {
   })
     .then(response => {
       if (response.status === 200) {
-        // const { refresh_token } = response.data;
-
-        // axios.get(`http://localhost:${port}/refresh_token?refresh_token=${refresh_token}`)
-        //   .then(response => {
-        //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-        //   })
-        //   .catch(error => {
-        //     res.send(error);
-        //   });
-        const { access_token, refresh_token } = response.data;
+        const { access_token, refresh_token, expires_in } = response.data;
 
         const queryParams = querystring.stringify({
           access_token,
           refresh_token,
+          expires_in,
         });
 
         res.redirect(`http://localhost:3000/?${queryParams}`);
 
       } else {
-        res.send(response);
+        res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
       }
     })
     .catch(error => {
