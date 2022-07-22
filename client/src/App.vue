b3814e2cba4bc0e70e92e29a3acab338f6d2be0d
Andres Diaz <andres@finhubtechnologies.com>
2022-07-22 16:10:43 +0100

    connect to spotify api

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,13 @@
     <div v-if="accessToken">
       <p>Logged in</p>
       <button @click="logout">log out</button>
+      <pre>{{ profile }}</pre>
+      <h1>{{ profile.display_name }}</h1>
+      <p>{{ profile.follower.total }} followers</p>
+      <img
+        v-if="profile.images.length" 
+        :src="profile.images[0].url"
+      />
     </div>
     <div v-else>
       <a href="http://localhost:8888/login">Log in to Spotify</a>
@@ -12,7 +19,15 @@
 
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import { accessToken, logout } from '@/use/spotify.js'
+import { computed } from 'vue';
+import { accessToken, logout, getCurrentUserProfile } from '@/use/spotify.js'
+
+const { data } = await getCurrentUserProfile()
+const profile = computed(() => data)
+
+console.log('data', data)
+console.log('profile', profile)
+
 </script>
 
 
