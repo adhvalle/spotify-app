872a0dc5c12081fc4bb678a77553575dcf95d3e5
Andres Diaz <andres@finhubtechnologies.com>
2022-07-26 16:21:04 +0100

    manage async calls

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div v-if="accessToken">
+      <p>Logged in</p>
+      <button @click="logout">log out</button>
+      <pre>{{ profile }}</pre>
+      <h1>{{ profile?.display_name }}</h1>
+      <p>{{ profile?.follower?.total }} followers</p>
+      <img
+        v-if="profile?.images?.length" 
+        :src="profile.images[0].url"
+      />
+    </div>
+    <div v-else>
+      <a href="http://localhost:8888/login">Log in to Spotify</a>
+    </div>
+  </div>
 </template>
+
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import { computed } from 'vue';
+import { accessToken, logout, getCurrentUserProfile } from '@/use/spotify.js'
+
+try {
+  const { data } = await getCurrentUserProfile()
+  const profile = computed(() => data || {})
+  
+  console.log('data', data)
+  console.log('profile', profile)
+} catch (e) {
+  console.error(e)
+} 
+</script>
